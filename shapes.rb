aee7dc0499148070800a61a6a8697a43361813b0
schwarro <rob.ap.schwartz@gmail.com>
2018-07-30 13:48:43 -0400

    completed shapes exercise

---
+++ shapes.rb
@@ -0,0 +1,27 @@
+def draw_shape(options)
+  shape = ""
+
+  options[:rows].times do |r|
+    options[:cols].times do |c|
+      shape += options[:char]
+    end
+    shape += "\n"
+  end
+
+  return shape
+
+end
+
+# options = {
+#   rows: 4,
+#   cols: 4,
+#   char: "*"
+# }
+
+options = {
+  rows: 3,
+  cols: 9,
+  char: "0"
+}
+
+puts draw_shape(options)
